82987cb27db4aa81101dd4931dcc42d380e6b17d
richman2020 <36261836+richman2020@users.noreply.github.com>
2022-09-18 22:35:45 +0430

    Update First-test.cy.js

--- First-test.cy.js
+++ First-test.cy.js
@@ -11,14 +11,14 @@ it("Questions No 1 To 3", () => {
     cy.get('input').invoke('val').should('not.be.empty')
 })
 
-it("Questions No 4", () => {
+it("Questions No 5", () => {
     cy.request('post', 'https://jsonplaceholder.typicode.com/todos', { "title": "foo", "body": "bar", "bar": "1" })
         .its('status')
         .should('eq', 201)
 
 })
 
-it("Questions No 5", () => {
+it("Questions No 4", () => {
 
     cy.request('GET', 'https://jsonplaceholder.typicode.com/todos').as('lord')
         .its('body')
