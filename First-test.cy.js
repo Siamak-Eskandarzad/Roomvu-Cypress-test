7d72162bff617715d707517b9bd5c1d9205ec0ba
richman2020 <36261836+richman2020@users.noreply.github.com>
2022-09-18 21:25:30 +0430

    Add files via upload

---
+++ First-test.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+
+
+it("Questions No 1 To 3", () => {
+    cy.visit("https://example.cypress.io/")
+    cy.get('h1').contains("Kitchen Sink")
+    cy.contains('type').click()
+    cy.url().should('include', '/commands/action')
+    cy.get('*[class^="form-control action-email"]').type('siamakeskandarzad@gmail.com')
+    cy.get('input').invoke('val').should('not.be.empty')
+})
+
+it("Questions No 4", () => {
+    cy.request('post', 'https://jsonplaceholder.typicode.com/todos', { "title": "foo", "body": "bar", "bar": "1" })
+        .its('status')
+        .should('eq', 201)
+
+})
+
+it("Questions No 5", () => {
+
+    cy.request('GET', 'https://jsonplaceholder.typicode.com/todos').as('lord')
+        .its('body')
+        .should(expect('@lord').to.be.not.null)
+
+})
+
